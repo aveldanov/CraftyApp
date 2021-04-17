c879aa9e214111879d22bc41484c5e29524a139a
Anton Veldanov <anton_veldanov@intuit.com>
2021-04-17 00:16:23 -0700

    iOS

--- CraftyApp/ViewModels/RepoViewModel.swift
+++ CraftyApp/ViewModels/RepoViewModel.swift
@@ -7,6 +7,34 @@
 
 import Foundation
 
+
+struct RepoListViewModel {
+    var repos: [Repo]
+}
+
+extension RepoListViewModel{
+    var numberOfSections: Int{
+        return 1
+    }
+    
+    var numberOfRowsInSection: Int{
+        return repos.count
+    }
+    
+    func repoAtIndex(_ index:Int) -> RepoViewModel{
+        let repo = repos[index]
+        return RepoViewModel(repo)
+    }
+    
+}
+
+
+
+
+
+
+
+
 struct RepoViewModel {
     private let repo: Repo
     
@@ -18,5 +46,19 @@ extension RepoViewModel{
         self.repo = repo
     }
     
+    var name: String?{
+        return repo.name
+    }
     
+    var description: String?{
+        return repo.description
+    }
+    
+    var watchers: Int?{
+        return repo.watchers
+    }
+    
+    var licsenseKey: String?{
+        return repo.license?.key
+    }
 }
