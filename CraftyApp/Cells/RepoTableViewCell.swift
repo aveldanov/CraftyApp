f42a77b283a09e3729d612aa8326fc769cc21328
Anton Veldanov <anton_veldanov@intuit.com>
2021-04-17 00:20:18 -0700

    iOS

---
+++ CraftyApp/Cells/RepoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RepoTableViewCell.swift
+//  CraftyApp
+//
+//  Created by Veldanov, Anton on 4/17/21.
+//
+
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
